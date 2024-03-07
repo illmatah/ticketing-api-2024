4bb4e9b60653ee8e38c9d666895006a8ff388729
illmatah <55992145+illmatah@users.noreply.github.com>
2024-03-07 18:21:07 +0000

    Chapter 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TicketsModule } from './tickets/tickets.module';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [],
+  imports: [
+    TicketsModule,
+    PrismaModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
